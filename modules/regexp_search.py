a253c70358944a1c69e608badf079a3232aa3f81
avimitin <avimitin@gmail.com>
2020-06-08 16:01:15 +0800

    💬 增加__init__调用使得程序实时更新关键词导入 🛠 重构类结构

--- modules/regexp_search.py
+++ modules/regexp_search.py
@@ -4,21 +4,25 @@
 import yaml
 import re
 
-with open("config/Reply.yml", 'r', encoding="UTF-8") as reply_msg_file:
-    reply_msg_dic = yaml.load(reply_msg_file, Loader=yaml.FullLoader)
-
 
 class Msg:
-    count = 0
-    reply_msg_list = list(reply_msg_dic.keys())
+
+    def __init__(self):
+        self.count = 0
+        
+        with open("config/Reply.yml", 'r', encoding="UTF-8") as reply_msg_file:
+            self.reply_msg_dic = yaml.load(reply_msg_file, Loader=yaml.FullLoader)
+
+        self.reply_msg_list = list(self.reply_msg_dic.keys())
 
     # 正则搜索
     def msg_match(self, lines):
+        self.__init__()
         # 计算回复的关键词字数
         count = len(self.reply_msg_list)-1
         # 关键词从列表最后一个开始匹配
         while count >= 0:
-            keywords = Msg.reply_msg_list[count]
+            keywords = self.reply_msg_list[count]
             msg_keywords_match = re.match(r'(.*)%s(.*)' % keywords, str(lines))
             if msg_keywords_match:
                 self.count = count
